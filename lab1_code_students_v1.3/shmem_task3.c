107f8e01aada9db7f5615fad92fa14aa67d8fd5b
itggot-lukas-einlerlarsson <lukas.einlerlarsson@itggot.se>
2023-02-11 17:17:42 +0100

    fixed task 3!

--- lab1_code_students_v1.3/shmem_task3.c
+++ lab1_code_students_v1.3/shmem_task3.c
@@ -1,97 +1,95 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <sys/types.h>
+#include <unistd.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
-#include <time.h>
-#include <stdlib.h>
-
-//semaphores
 #include <pthread.h>
+#include <stdlib.h>
 #include <semaphore.h>
+#include <errno.h>
+#include <sys/wait.h>
 #include <fcntl.h>
 
+#define N 10
 #define SHMSIZE 128
 #define SHM_R 0400
 #define SHM_W 0200
 
-#define N 10
+const char *semName1 = "my_sema_7";
+const char *semName2 = "my_sema_8";
 
-//useed to get a sleeptime of 0.1-2 milliseconds
-void msecSleepParent();
-void msecSleepChild();
+struct shm_struct {
+    int buffer[N];
+    int start;
+	int end;
+};
 
-
-int main(int argc, char **argv)
-{
-	/*	cirucular buffer , inspiration from s 142 in coursebook.*/
-	struct shm_struct {
-		int buffer[N];
-		int index;
-		int amount;
-		//int* front = buffer, rear = front; //starts as empty
-	};
+int main() {
 	volatile struct shm_struct *shmp = NULL;
 	char *addr = NULL;
 	pid_t pid = -1;
-	int var1 = 0, var2 = 0, shmid = -1;
+	int var1 = 0, var2 = 0, shmid = -1, status;
 	struct shmid_ds *shm_buf;
 
-	//for sync of processess via semaphores:
-	const char *semName1 = "sema1";
-	const char *semName2 = "sema2";
-	sem_t *sem_id1 = sem_open(semName1, O_CREAT, O_RDWR, 10);
+    /* initalize semaphores, first one to the size of the array */
+	sem_t *sem_id1 = sem_open(semName1, O_CREAT, O_RDWR, N);
 	sem_t *sem_id2 = sem_open(semName2, O_CREAT, O_RDWR, 0);
-
 
 	/* allocate a chunk of shared memory */
 	shmid = shmget(IPC_PRIVATE, SHMSIZE, IPC_CREAT | SHM_R | SHM_W);
 	shmp = (struct shm_struct *) shmat(shmid, addr, 0);
 	pid = fork();
 
-	shmp->index = 0;
-	shmp->amount = 0;
-	//used to get random nubmer for millisecond-sleep.
-	srand(time(NULL));
+    srand(time(NULL));
 
-	if (pid != 0) {
+    if (pid != 0) {
 		/* here's the parent, acting as producer */
 		while (var1 < 100) {
 			/* write to shmem */
-			sem_wait(sem_id1); //wait until region is unlocked -> added to semaphore queue
+			// Decrease value (write), if 0 (we've written up to the consumer, we wait)
+			sem_wait(sem_id1);
 			var1++;
 
 			printf("Sending %d\n", var1); fflush(stdout);
-			shmp->buffer[shmp->index] = var1;
-			shmp->amount++;
-			shmp->index = (shmp->index+1) % N; //making it circular, bounded by N(=10)
+			shmp->buffer[shmp->end] = var1;
+			shmp->end = (shmp->end+1) % N;
 
-			msecSleepParent();
+			msecSleepParent()
 
-			sem_post(sem_id2);//unlocks region -> waiting process can now enter.
+			// Post sem_id2 to increase semaphore value (allow it to start reading)
+			sem_post(sem_id2);
 		}
 		shmdt(addr);
 		shmctl(shmid, IPC_RMID, shm_buf);
+    	sem_close(sem_id1);
+		sem_close(sem_id2);
+		wait(&status);
+		sem_unlink(semName1);
+		sem_unlink(semName2);
 	} else {
 		/* here's the child, acting as consumer */
 		while (var2 < 100) {
-			/* read from shmem */
 			sem_wait(sem_id2);
 
-			var2 = shmp->buffer[shmp->index];
-			shmp->index = (shmp->index+1) % N; //making it circular, bounded by N(=10)
-			shmp->amount++;
-			printf("Received %d\n", var2); fflush(stdout);
+            var2 = shmp->buffer[shmp->start];
+            shmp->start = (shmp->start+1) % N;
+            printf("\tReceived %d\n", var2); fflush(stdout);
 
-			msecSleepChild();
-			
-			sem_post(sem_id1);
+            msecSleepChild()
+
+            // Post sem_id1 to increase semaphore value (allow it to write more, can be up to 10, so if we read more, it is allowed to write more)
+            sem_post(sem_id1);
 		}
 		shmdt(addr);
 		shmctl(shmid, IPC_RMID, shm_buf);
+    	sem_close(sem_id1);
+		sem_close(sem_id2);
 	}
+
+    return 0;
 }
+
 
 void msecSleepParent(){
 	int random = rand() % 500;
